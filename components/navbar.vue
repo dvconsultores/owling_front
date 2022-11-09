4a6f9a0a8dd4b410a3f6f74b3b5a52c7dd3b4303
detextre4 <detextre4@gmail.com>
2022-11-09 12:02:56 -0400

    initial commit

---
+++ components/navbar.vue
@@ -0,0 +1,84 @@
+<template>
+  <div>
+    <MenuNavbar ref="menu"></MenuNavbar>
+    <v-app-bar id="navbar" color="transparent" absolute class="isolate">
+      <nuxt-link to="/">
+        <img src="~/assets/sources/logos/logo.svg" alt="logo" style="--w: clamp(10em, 13vw, 13.414375em)">
+      </nuxt-link>
+
+      <aside class="middle tcap deletemobile">
+        <a
+          v-for="(item, i) in dataNavbar" :key="i"
+          :class="{active: item.to === $route.path}"
+          @click="$router.push(localePath(item.to))">
+          {{item.name}}
+        </a>
+      </aside>
+
+      <aside class="right">
+        <v-btn class="btn">
+          <span>${{user.balance}}</span>
+        </v-btn>
+        
+        <v-btn v-show="!isLogged" class="btn" @click="$store.commit('signIn')">Connect wallet</v-btn>
+        <v-btn v-show="isLogged" class="btn openMenuLogin">
+          <span>{{user.accountId}}</span>
+          <v-icon>mdi-chevron-down</v-icon>
+        </v-btn>
+      </aside>
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+import computeds from '~/mixins/computeds'
+
+export default {
+  name: "NavbarComponent",
+  mixins: [computeds],
+  data() {
+    return {
+      dataNavbar: [
+        {
+          name: "portfolio",
+          to: ""
+        },
+        {
+          name: "swap",
+          to: ""
+        },
+        {
+          name: "farm",
+          to: ""
+        },
+        {
+          name: "pools",
+          to: ""
+        },
+        {
+          name: "xhpot",
+          to: ""
+        },
+      ],
+    };
+  },
+  // created() {
+  //   const theme = localStorage.getItem("theme");
+  //   if (theme) {
+  //     setTimeout(() => {
+  //       this.$store.commit("switchTheme", theme);
+  //     }, 100);
+  //   }
+  //   if (theme === "light") {this.themeButton = true}
+  //   else {this.themeButton = false}
+  // },
+  methods: {
+    // changeTheme(theme) {
+    //   this.$store.commit("switchTheme", theme);
+    //   this.themeButton = !this.themeButton;
+    // },
+  },
+};
+</script>
+
+<style src="~/assets/styles/components/navbar.scss" lang="scss" />
---
+++ pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="home" class="divcol">
+    <h1 class="hover_split maxsize_w">WHO ARE YOU?
+      <span class="mask"><span>WHO ARE YOU?</span></span>
+      <span class="mask"><span>WHO ARE YOU?</span></span>
+    </h1>
+  </div>
+</template>
+
+<script>
+import computeds from '~/mixins/computeds'
+
+export default {
+  name: "HomePage",
+  mixins: [computeds],
+  data() {
+    return {
+    }
+  },
+  head() {
+    const title = 'Home';
+    return {
+      title,
+    }
+  },
+  methods: {
+  }
+};
+</script>
+
+<style src="~/assets/styles/pages/index.scss" lang="scss" />
