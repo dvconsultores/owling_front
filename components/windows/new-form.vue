7ac271d5178e3985656cf20fb240d858dd28803d
Juan Ochando <juanochando00@gmail.com>
2022-11-18 12:16:47 -0400

    up

--- components/windows/new-form.vue
+++ components/windows/new-form.vue
@@ -779,7 +779,7 @@ export default {
             datos.results_images.push(this.formZero.nfts[i].url)
           }
           localStorage.tipohash = 'form'
-          await contract.create_form(datos,'300000000000000', "90000000000000000000000")
+          await contract.create_form(datos,'300000000000000', "30000000000000000000000")
           .then((response) => {
             this.$refs.modal.openModal('success')
           }).catch((error) => {
